0833621ca754f135571847a26101a3b153a8733b
Xavier De Leon <xdeleon@gmail.com>
2026-01-04 21:27:51 -0800

    Initial Commit

---
+++ SupabaseTest/SupabaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseTestApp.swift
+//  SupabaseTest
+//
+//  Created by Xavier De Leon on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
